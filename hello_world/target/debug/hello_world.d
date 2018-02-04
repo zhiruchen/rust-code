0f0ac0d6b150854b4c3aa7c82e3d42ec7ec3fc57
zhiruchen <zhiruchen1992@foxmail.com>
2018-02-04 11:31:58 +0800

    hello world && rust

---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/zhiruchen/Documents/Code/github/rust-code/hello_world/target/debug/hello_world.dSYM: /Users/zhiruchen/Documents/Code/github/rust-code/hello_world/src/main.rs
---
+++ hello_world/target/release/hello_world.d
@@ -0,0 +1,1 @@
+/Users/zhiruchen/Documents/Code/github/rust-code/hello_world/target/release/hello_world.dSYM: /Users/zhiruchen/Documents/Code/github/rust-code/hello_world/src/main.rs
