faac6132a849f6889f09f71efb39390202c9168a
zhiruchen <zhiruchen1992@foxmail.com>
2018-02-04 13:55:04 +0800

    add guessing game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,39 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("the secret_number is: {}", secret_number);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();  // 创建一个可变绑定guess
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,  // 成功解析
+            Err(_) => continue,  // 错误，继续
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+    
+}
