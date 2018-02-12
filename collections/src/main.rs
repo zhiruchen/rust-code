a35b01f98f9e44038afcb1b8ae0ceeeb732d6fb5
zhiruchen <zhiruchen1992@foxmail.com>
2018-02-12 10:52:49 +0800

    add string op

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -53,4 +53,39 @@ fn enum_vec(){
     ];
 
     println!("{:?}", row);
+
+    // 字符串
+    op_string();
+}
+
+fn op_string() {
+    let mut s = String::new();
+    println!("s.len: {}", s.len());
+
+    let data = "init string";
+    let s1 = data.to_string();
+    println!("s1: {}", s1);
+
+    println!("string from to_string: {}", "init content".to_string());
+
+    println!("from string: {}", String::from(s1));
+
+    // 增长字符串
+    let mut s2 = String::from("from string");
+    s2.push_str("spring");
+    s2.push_str("festival");
+    println!("{}", s2);
+
+    let mut s3 = String::from("lo");
+    s3.push('l');
+    println!("s3: {}", s3);
+
+    let str_data1 = "hello ".to_string();
+    let str_data2 = "world!".to_string();
+    let str_data3 = str_data1 + &str_data2;
+    println!("{}", str_data3);
+
+    // 连接字符串
+    let str_data4 = format!("{}-{}-{}", "tic".to_string(), "tao".to_string(), "toc".to_string());
+    println!("{}", str_data4);
 }
