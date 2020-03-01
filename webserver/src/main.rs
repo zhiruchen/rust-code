35ad9b7385fd7a13f3498ba63b8f0c836aa5ebbb
zhiruchen <zhiruchen1992@foxmail.com>
2020-03-01 23:46:13 +0800

    update handle connection

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::io::prelude::*;
 use std::net::{ TcpListener, TcpStream};
 
@@ -15,4 +16,18 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, file_name) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "page.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+
+    let contents = fs::read_to_string(file_name).unwrap();
+
+    let resp = format!("{}{}", status_line, contents);
+    stream.write(resp.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
