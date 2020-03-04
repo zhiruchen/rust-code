174bacac9ea6f75da58fb7538c2a2126507ddff9
zhiruchen <zhiruchen1992@foxmail.com>
2020-03-05 00:27:01 +0800

    thread pool of webserver-2(workers)

--- webserver/src/lib.rs
+++ webserver/src/lib.rs
@@ -1,19 +1,19 @@
 use std::thread;
 
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
 }
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
-        let mut threads = Vec::with_capacity(size);
-        for _ in 0..size {
-
+        let mut workers = Vec::with_capacity(size);
+        for id in 0..size {
+            workers.push(Worker::new(id))
         }
         ThreadPool {
-            threads
+            workers
         }
     }
 
@@ -22,5 +22,20 @@ impl ThreadPool {
            F: FnOnce() + Send + 'static
     {
 
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    pub fn new(id: usize) Worker {
+        let thread = thread::spawn(|| {});
+        Worker {
+            id,
+            thread;
+        }
     }
 }
