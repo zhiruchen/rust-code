4dd5ab366f3cbb7652f74fd7793e09090057fdae
zhiruchen <zhiruchen1992@foxmail.com>
2020-03-04 23:58:17 +0800

    thread pool of webserver

--- webserver/src/bin/main.rs
+++ webserver/src/bin/main.rs
@@ -2,12 +2,19 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::{ TcpListener, TcpStream};
 
+use webserver::ThreadPool;
+
 fn main() {
     let lisener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in lisener.incoming(){
         let stream = stream.unwrap();
-        handle_connection(stream);
+
+        pool::execute(|| {
+            handle_connection(stream);
+        });
+        
         println!("Connection is established");
     }
 }
---
+++ webserver/src/lib.rs
@@ -0,0 +1,15 @@
+pub struct ThreadPool;
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+        ThreadPool
+    }
+
+    pub fn execute<F>(&self, f: F)
+       where
+           F: FnOnce() + Send + 'static
+    {
+
+    }
+}
